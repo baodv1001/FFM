785d626211785760e8636e2b6b59b0672f8d9aab
quangpn0204 <70959574+quangpn0204@users.noreply.github.com>
2020-10-19 00:22:41 +0700

    Login/Init login feature
    - Xử lý các thao tác đăng nhập
    - Chỉnh sửa class account và employee trong folder Models

---
+++ FootballFieldManagement/FootballFieldManagement/DAL/AccountDal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FootballFieldManagement.Models;
+
+namespace FootballFieldManagement.DAL
+{
+    class AccountDal : DataProvider
+    {
+        List<Account> listAccount;
+        internal List<Account> ListAccount { get => listAccount; set => listAccount = value; }
+        public AccountDal()
+        {
+            ListAccount = new List<Account>();
+            DataTable dt = LoadData("Account");
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Account acc = new Account(int.Parse(dt.Rows[i].ItemArray[0].ToString()), dt.Rows[i].ItemArray[1].ToString(), dt.Rows[i].ItemArray[2].ToString(), int.Parse(dt.Rows[i].ItemArray[3].ToString()));
+                listAccount.Add(acc);
+            }
+        }
+    }
+}
